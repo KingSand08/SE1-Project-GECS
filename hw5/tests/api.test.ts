1b4a94a08c0e9ebd6c93f9874468c1585c000e17
gursheyss <rkwarya@gmail.com>
2023-11-26 14:02:32 -0800

    test

--- hw5/tests/api.test.ts
+++ hw5/tests/api.test.ts
@@ -6,7 +6,6 @@ test('GET weather data', async () => {
 
     const request = { url: `http://api.weatherapi.com/v1/current.json?key=f473fed20e1641d0b8011337232411&q=sanjose` }
     const response = await getWeather(request)
-
     const responseData = await response.json()
 
     expect(responseData).toEqual({ body: { error: { code: 1006, message: "No matching location found." } } })
