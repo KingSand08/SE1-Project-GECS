79301969e327ea027cb2fb0a4c4e909fd0880997
gursheyss <rkwarya@gmail.com>
2023-11-26 13:39:00 -0800

    env

--- hw5/src/routes/api/+server.ts
+++ hw5/src/routes/api/+server.ts
@@ -1,10 +1,9 @@
 import type { RequestHandler } from "@sveltejs/kit";
-import { WEATHERAPI_API_KEY } from "$env/static/private";
 
 export const GET: RequestHandler = async (request) => {
     const location = new URL(request.url).searchParams.get('q')
     const response = await fetch(
-        `http://api.weatherapi.com/v1/current.json?key=${WEATHERAPI_API_KEY}&q=${location}`
+        `http://api.weatherapi.com/v1/current.json?key=f473fed20e1641d0b8011337232411&q=${location}`
     );
     const weatherData = await response.json();
     return new Response(JSON.stringify({ body: weatherData }), {
